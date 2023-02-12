ca876bb07d7302d99c363fe3175bb8bf1d20dae0
sayantalukdar <sayan.talukdar@nagarro.com>
2023-02-12 20:26:16 +0530

    Changes

--- Jenkins_FreeStyle_Demo/Jenkins_FreeStyle_Demo/Program.cs
+++ Jenkins_FreeStyle_Demo/Jenkins_FreeStyle_Demo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+//using Microsoft.Extensions.Logging;
 
 namespace Jenkins_FreeStyle_Demo
 {
@@ -9,6 +10,7 @@ namespace Jenkins_FreeStyle_Demo
         {
             var counter = 0;
             var max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
+            Console.WriteLine("Counter starts from here:");
             while (max == -1 || counter < max)
             {
                 Console.WriteLine($"Counter number: {++counter}");
