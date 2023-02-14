01564584eaa1dc61cfa7e33c935dd85dd98e4f52
sayantalukdar <sayan.talukdar@nagarro.com>
2023-02-14 22:41:48 +0530

    jenkins chnge

---
+++ Jenkins_Pipeline_Demo/Jenkins_Pipeline_Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jenkins_Pipeline_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
